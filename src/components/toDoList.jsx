08a82a6b1d854015c51c4086166704486457bca0
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-01-28 13:11:23 +0200

    update env

--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -1,11 +1,10 @@
 import { db } from '../appwriteConfig'
 import { ID } from 'appwrite'
-import { pass } from '../pass'
 export const upDateTask = async (data) => {
     try {
         const response = await db.updateRow({
-            databaseId: pass.VITE_APPWRITE_DB_ID
-            , tableId: pass.VITE_APPWRITE_TABLE_TODO_lIST,
+            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
+            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST,
             rowId: data.$id,
             data: data
         });
@@ -19,8 +18,8 @@ export const upDateTask = async (data) => {
 export const addTask = async (data) => {
     try {
         const response = await db.createRow({
-            databaseId: pass.VITE_APPWRITE_DB_ID
-            , tableId: pass.VITE_APPWRITE_TABLE_TODO_lIST,
+            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
+            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST,
             rowId: ID.unique(),
             data: data
         });
@@ -33,8 +32,8 @@ export const addTask = async (data) => {
 export const deleteTask = async (data) => {
     try {
         const response = await db.deleteRow({
-            databaseId: pass.VITE_APPWRITE_DB_ID
-            , tableId: pass.VITE_APPWRITE_TABLE_TODO_lIST,
+            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
+            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST,
             rowId: data.$id,
         });
         console.log(response)
@@ -50,8 +49,8 @@ export const getTasks = async () => {
 
         //total rows
         const { total, rows } = await db.listRows({
-            databaseId: pass.VITE_APPWRITE_DB_ID
-            , tableId: pass.VITE_APPWRITE_TABLE_TODO_lIST
+            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
+            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST
         })
 
 
