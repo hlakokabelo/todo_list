12fbf3a4d19fb42680b3985729b18a9f9e09658b
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-01-28 15:44:22 +0200

    more modularity added

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,19 @@
 import { Client, TablesDB } from "appwrite";
 
+
+// @desc client credentials
+export const clientCred = {
+  PROJECT_ID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  PROJECT_NAME: import.meta.env.VITE_APPWRITE_PROJECT_NAME,
+  ENDPOINT: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  DB_ID: import.meta.env.VITE_APPWRITE_DB_ID,
+  TABLE_ID_TO_DO_LIST: import.meta.env.VITE_APPWRITE_TABLE_ID_TO_DO_lIST,
+};
+
 const client = new Client();
 
-client.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT);
-client.setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+client.setEndpoint(clientCred.ENDPOINT);
+client.setProject(clientCred.PROJECT_ID);
 
 const db = new TablesDB(client);
 
--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -1,11 +1,11 @@
-import { db } from '../appwriteConfig'
+import { clientCred, client, db, } from '../appwriteConfig'
 import { ID } from 'appwrite'
 
 export const upDateTask = async (data) => {
     try {
         const response = await db.updateRow({
-            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
-            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST,
+            databaseId: clientCred.DB_ID
+            , tableId: clientCred.TABLE_ID_TO_DO_LIST,
             rowId: data.$id,
             data: data
         });
@@ -15,42 +15,43 @@ export const upDateTask = async (data) => {
 
     }
 }
+
+
 export const addTask = async (data) => {
     try {
         const response = await db.createRow({
-            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
-            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST,
+            databaseId: clientCred.DB_ID
+            , tableId: clientCred.TABLE_ID_TO_DO_LIST,
             rowId: ID.unique(),
             data: data
         });
         return response
     } catch (error) {
-       return (error)
+        return (error)
 
     }
 }
 export const deleteTask = async (data) => {
     try {
         const response = await db.deleteRow({
-            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
-            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST,
+            databaseId: clientCred.DB_ID
+            , tableId: clientCred.TABLE_ID_TO_DO_LIST,
             rowId: data.$id,
         });
-        console.log(response)
         return response
     } catch (error) {
-        console.log(error)
+        return (error)
 
     }
 }
 
 export const getTasks = async () => {
     try {
-    
+
         //total rows
         const { total, rows } = await db.listRows({
-            databaseId: import.meta.env.VITE_APPWRITE_DB_ID
-            , tableId: import.meta.env.VITE_APPWRITE_TABLE_TODO_lIST
+            databaseId: clientCred.DB_ID
+            , tableId: clientCred.TABLE_ID_TO_DO_LIST,
         })
 
 
